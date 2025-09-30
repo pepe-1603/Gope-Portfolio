39f0e55b10920d05fe0aa004b8f47c511016cade
pepe1603 <b180022@unach.mx>
2025-09-30 13:33:26 -0600

    Revert "fix(vite): Establece base: '/' en config.ts para resolver conflictos de rutas y MIME en Netlify."
    This reverts commit 308b94536be09b3299b4d7af0dbc968ca11b364e.

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // ✅ FIX: Asegura que todas las rutas se generen con base en la raíz del dominio
-  base: '/',
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
